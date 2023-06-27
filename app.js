5a4667afb0a9d3aaf61e1180db1732d03930a260
Abheekrishna <abheekrishna17@gmail.com>
2023-06-27 20:57:01 +0530

    Changed code

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 const nameInput = document.getElementById('name');
 const passwordInput = document.getElementById('password');
 const btn = document.getElementById('button');
+const container = document.querySelector('.container');
+const para = document.createElement('p');
+para.classList.add('para')
 
 btn.addEventListener('mouseover', (button) => {
     let name = nameInput.value;
     let password = passwordInput.value;
-
     if(name === '' || password === '') {
-        button.target.classList.toggle('move')
+        move();
+        nameInput.style.border = '1px solid red';
+        passwordInput.style.border = '1px solid red';
     } else {
-        button.target.classList.remove('move');
-        button.target.addEventListener('click', () => {
-            alert('Submitted')
+        nameInput.style.border = '1px solid green';
+        passwordInput.style.border = '1px solid green';
+        btn.addEventListener('click', () => {
+            
+            para.innerText = 'Done!!';
+            container.append(para);
         })
     }
+
+
 })
+
+let flag = 1;
+
+function move() {
+    if(flag === 1) {
+
+    btn.style.transform = 'translateX(250%)'
+    flag = 2;
+    } else if(flag === 2) {
+        btn.style.transform = 'translateX(0%)';
+        flag = 1;
+    }
+}
